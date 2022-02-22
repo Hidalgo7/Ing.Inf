537b8348dcfb14335289ef4ae3ff00042d693aed
Hidalgo7 <ikerhidalgo7@gmail.com>
2022-02-22 12:56:47 +0100

    San Valentin Solitario

--- IA/PL1b/search.py
+++ IA/PL1b/search.py
@@ -4,10 +4,11 @@
 In search.py, you will implement generic search algorithms which are called by
 Pacman agents (in searchAgents.py).
 """
-
+import copy
 from time import sleep
 from tkinter import E
 import util
+
 
 class SearchProblem:
     """
@@ -60,7 +61,8 @@ def tinyMazeSearch(problem):
     from game import Directions
     s = Directions.SOUTH
     w = Directions.WEST
-    return  [s, s, w, s, w, w, s, w]
+    return [s, s, w, s, w, w, s, w]
+
 
 def depthFirstSearch(problem):
     """
@@ -80,19 +82,19 @@ def depthFirstSearch(problem):
     from game import Directions
     from util import Stack
     movements = {
-        'South' : Directions.SOUTH,
-        'West' : Directions.WEST,
-        'East' : Directions.EAST,
-        'North' : Directions.NORTH
+        'South': Directions.SOUTH,
+        'West': Directions.WEST,
+        'East': Directions.EAST,
+        'North': Directions.NORTH
     }
 
     path = [problem.getStartState()]
     checked = [problem.getStartState()]
     porExaminar = Stack()
     for successor in problem.getSuccessors(problem.getStartState()):
-        porExaminar.push((problem.getStartState(),successor))
+        porExaminar.push((problem.getStartState(), successor))
     reached = False
-    
+
     while porExaminar and not reached:
         next = porExaminar.pop()
         while next[1][0] in checked and not porExaminar.isEmpty():
@@ -105,19 +107,20 @@ def depthFirstSearch(problem):
             reached = True
         else:
             for successor in problem.getSuccessors(next[1][0]):
-                porExaminar.push((next[1][0],successor))
-    
+                porExaminar.push((next[1][0], successor))
+
     result = []
-    for i in range(0,len(path)-1):
+    for i in range(0, len(path) - 1):
         origin = path[i]
-        goal = path[i+1]
+        goal = path[i + 1]
         action = [x for x in problem.getSuccessors(origin) if x[0] == goal]
         result.append(action[0][1])
 
     return result
 
+
 def breadthFirstSearch(problem):
-    """Search the shallowest nodes in the search tree first."""
+    """Search the node of least total cost first."""
     "*** YOUR CODE HERE ***"
     from game import Directions
     from util import Queue
@@ -128,40 +131,32 @@ def breadthFirstSearch(problem):
         'North' : Directions.NORTH
     }
 
-    paths = [(problem.getStartState(),problem.getStartState())]
     checked = [problem.getStartState()]
 
     porExaminar = Queue()
-    for successor,_,_ in problem.getSuccessors(problem.getStartState()):
+    caminos = Queue()
+
+    for successor,dir,_ in problem.getSuccessors(problem.getStartState()):
         porExaminar.push(successor)
-        paths.append((problem.getStartState(),successor))
+        caminos.push([dir])
     
-    goal = (0,0)
-    
-    while porExaminar and goal == (0,0):
+    while not porExaminar.isEmpty():
         next = porExaminar.pop()
-        print("Start's successors:", problem.getSuccessors(next))
-        checked.append(next)
+        path = caminos.pop()
+        print(path)
         if problem.isGoalState(next):
-            goal = next
-        else:
-            for successor,_,_ in problem.getSuccessors(next):
+                return path
+        elif next not in checked:
+            checked.append(next)
+            for successor,dir,_ in problem.getSuccessors(next):
                 if successor not in checked:
-                    paths.append((next,successor))
+                    thisPath = path + [dir]
                     porExaminar.push(successor)
-                
+                    caminos.push(thisPath)
+                else:
+                    print("Checked : {}".format(successor))
     
-    result = []
-    movimiento = [x for x in paths if x[1] == goal][0]
-
-    while movimiento[0] != movimiento[1]:
-        origin = movimiento[0]
-        destiny = movimiento[1]
-        action = [x for x in problem.getSuccessors(origin) if x[0] == destiny][0]
-        result.insert(0,movements[action[1]])
-        movimiento = [x for x in paths if x[1] == origin][0]
-
-    return result
+    return []
 
 def uniformCostSearch(problem):
     """Search the node of least total cost first."""
@@ -169,23 +164,23 @@ def uniformCostSearch(problem):
     from game import Directions
     from util import PriorityQueue
     movements = {
-        'South' : Directions.SOUTH,
-        'West' : Directions.WEST,
-        'East' : Directions.EAST,
-        'North' : Directions.NORTH
+        'South': Directions.SOUTH,
+        'West': Directions.WEST,
+        'East': Directions.EAST,
+        'North': Directions.NORTH
     }
 
     paths = PriorityQueue()
-    paths.push(([problem.getStartState],0))
+    paths.push(([problem.getStartState], 0))
     checked = [problem.getStartState()]
 
     reached = False
-    
+
     while not reached and not paths.isEmpty():
         next = paths.pop()
-        for successor,_,cost in problem.getSuccessors(next[0]):
-            new_path = (next[0].append(successor),next[1] + cost)
-            paths.push(new_path,new_path[1])
+        for successor, _, cost in problem.getSuccessors(next[0]):
+            new_path = (next[0].append(successor), next[1] + cost)
+            paths.push(new_path, new_path[1])
         # while  in checked and not porExaminar.isEmpty():
         #     next = porExaminar.pop()
         # while path[-1] != next[0]:
@@ -197,7 +192,7 @@ def uniformCostSearch(problem):
         # else:
         #     for successor in problem.getSuccessors(next[1][0]):
         #         porExaminar.push((next[1][0],successor))
-    
+
     # result = []
     # for i in range(0,len(path)-1):
     #     origin = path[i]
@@ -207,6 +202,7 @@ def uniformCostSearch(problem):
 
     # return result
 
+
 def nullHeuristic(state, problem=None):
     """
     A heuristic function estimates the cost from the current state to the nearest
@@ -214,10 +210,88 @@ def nullHeuristic(state, problem=None):
     """
     return 0
 
+
 def aStarSearch(problem, heuristic=nullHeuristic):
     """Search the node that has the lowest combined cost and heuristic first."""
     "*** YOUR CODE HERE ***"
-    util.raiseNotDefined()
+    from game import Directions
+    from util import Queue
+    movements = {
+        'South': Directions.SOUTH,
+        'West': Directions.WEST,
+        'East': Directions.EAST,
+        'North': Directions.NORTH
+    }
+
+    # ESTRUCTURE OF GETSUCCESORS: [((X,Y) , DIRECTION, COST), ((X,Y) , DIRECTION, COST)]
+
+    # ESTRUCTURE OF PARTIAL PATHS ELEMENTS: [ ([NODEA, NODEB, NODEC, ....], (INT) COST,  [DIR1, DIR2, DIR3, ...]) ,
+    # ([NODEA, NODEB, NODED, ....], (INT) COST,  [DIR1, DIR2, DIR3, ...])]
+    # --> MUST BE ORDERED BY COST
+
+    partialPaths = []
+    rootNode = problem.getStartState()
+    alreadyExpanded = [rootNode]
+
+    for successor, direction, cost in problem.getSuccessors(rootNode):
+        partialPaths.append(([rootNode, successor], cost + heuristic(successor, problem),[direction]))
+
+    partialPaths = orderPathsByCost(partialPaths)
+    # print("\nCamino inicial: {}".format(partialPaths))
+
+    caminoOptimo = False
+
+    while (not caminoOptimo) and partialPaths:
+        lowestCostPath = partialPaths.pop(0)
+        # print("Lowest path: {}".format(lowestCostPath))
+
+        actualNode = lowestCostPath[0][-1]
+
+        # print("\nActual node {} and his successors {}:".format(actualNode,problem.getSuccessors(actualNode)))
+        alreadyExpanded.append(actualNode)
+        for successor, direction, cost in problem.getSuccessors(actualNode):
+            if successor not in lowestCostPath[0] and successor not in alreadyExpanded:
+                # ([NODEA, NODEB, NODEC, ....], (INT) COST, [DIR1, DIR2, DIR3, ...])
+                newPath = copy.deepcopy(lowestCostPath[0])
+                newPath.append(successor)
+                newCost = lowestCostPath[1] + cost + heuristic(successor, problem)
+                newPartialDir = copy.deepcopy(lowestCostPath[2])
+                newPartialDir.append(direction)
+                newPartialPath = (newPath, newCost, newPartialDir)
+                add = caminosRepetidos(partialPaths, newPartialPath)
+                if add:
+                    partialPaths.insert(-1, newPartialPath)
+        partialPaths = orderPathsByCost(partialPaths)
+        # print("\nCamino despues de insertar sucesores: {}".format(partialPaths))
+
+        # print((partialPaths[0][0][len(partialPaths[0][0])-1]))
+        if problem.isGoalState(partialPaths[0][0][-1]):  # primer elemento ultimo nodo
+            caminoOptimo = True
+
+    if caminoOptimo:
+        result = []
+        caminoElegido = partialPaths[0][2]
+        return caminoElegido
+    else:
+        return Exception("No hay camino posible al objetivo. ")
+
+
+def orderPathsByCost(partialPaths):
+    # ESTRUCTURE OF PARTIAL PATHS ELEMENTS: [ ([NODEA, NODEB, NODEC, ....], (INT) COST,  [DIR1, DIR2, DIR3, ...]) ,
+    # ([NODEA, NODEB, NODED, ....], (INT) COST,  [DIR1, DIR2, DIR3, ...])]
+    return sorted(partialPaths, key=lambda cost: cost[1])
+
+
+def caminosRepetidos(partialPaths, newPartialPath):
+    add = True
+    for camino in partialPaths:
+        if camino[0][-1] == newPartialPath[0][-1]:
+            if camino[1] > newPartialPath[1]:
+                partialPaths.remove(camino)
+            else:
+                add = False
+            break
+    return add
 
 
 # Abbreviations
